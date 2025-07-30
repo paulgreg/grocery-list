c3b4cc8a4a55bfc95d942bcef39c2edf685e2ba4
Grégory Paul <me@gpaul.me>
2025-07-30 14:53:40 +0200

    fix: serviceworker

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,10 @@ import App from './App.tsx'
 import HomePage from './Pages/HomePage.tsx'
 import ListPage from './Pages/ListPage.tsx'
 
+if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/grocery-list/sw.js')
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <BrowserRouter
